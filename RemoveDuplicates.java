e22144a4cb208c487435955ff2f496f0d8dd8f7c
VaishaliMathi <112792215+VaishaliMathi@users.noreply.github.com>
2022-09-22 20:51:17 +0530

    Add files via upload

---
+++ RemoveDuplicates.java
@@ -0,0 +1,30 @@
+package week3.day1;
+
+public class RemoveDuplicates 
+{
+
+	public static void main(String[] args) 
+	{
+		String text="we learn java basics as part of java session oF learn in java week1";
+        String[] split= text.split(" ");
+        int count =0; 
+        for(int i=0;i<split.length;i++) 
+        {
+            for(int j=i+1;j<split.length;j++) 
+            {
+                if(split[i].equalsIgnoreCase(split[j]))
+                {
+                    split[j]="";
+                    count++;
+                }
+            }
+        }
+        if(count>1) 
+        {
+            for(int k=0;k<split.length;k++)
+            {
+                System.out.print(split[k]+" ");
+            }  
+        }
+    }
+}
